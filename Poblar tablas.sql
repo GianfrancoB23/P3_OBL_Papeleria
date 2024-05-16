2830e8ea349e7969cabfd4935dac1dce4fd62cf8
Gianfranco Bonanni <110034721+GianfrancoB23@users.noreply.github.com>
2024-05-16 10:07:21 -0300

    poblar tablas

--- Poblar tablas.sql
+++ Poblar tablas.sql
@@ -14,20 +14,20 @@ insert into Usuarios (Contrasenia_Valor, Email_Direccion, NombreCompleto_Apellid
 ('Prueba123!', 'emarsiglia@email.com', 'Marsiglia', 'Esteban')
 
 insert into Articulos(PrecioVP,CodigoProveedor_codigo, Descripcion_Descripcion, NombreArticulo_Nombre,Stock_cantidad) values
-(2500,1234567891231, 'Hojas de tamaño A4', 'Hoja A4',5000),
-(1500, 9876543210981, 'Tijeras escolares', 'Tijeras', 2000),
-(5000, 4561237894561, 'Cuadernos de 100 hojas', 'Cuaderno', 3000),
-(3500, 7891234567891, 'Lápices HB', 'Lápiz HB', 10000),
-(10000,3216549871231, 'Mochilas escolares', 'Mochila', 500),
-(7500,6549873214561, 'Gomas de borrar', 'Goma de borrar', 8000),
-(25000,7894561237819, 'Calculadoras científicas', 'Calculadora', 1500),
-(2000, 1237896543121, 'Cartulinas de colores', 'Cartulina', 6000),
-(4500, 4567891231987, 'Pegamento en barra', 'Pegamento', 7000),
-(1800, 1122334451566, 'Bolígrafos azules', 'Bolígrafo', 12000),
-(4000, 2233445516677, 'Resaltadores', 'Resaltador', 8000),
-(3500, 3344556617788, 'Libretas de notas', 'Libreta', 5000),
-(1500, 4455667718899, 'Grampas para engrapadora', 'Grampas', 15000),
-(500, 5566778891900, 'Notas adhesivas', 'Post-it', 9000);
+(500,1234567891231, 'Pack Hojas de tamaño A4', 'Hoja A4',5000),
+(50, 9876543210981, 'Tijeras escolares', 'Tijeras', 2000),
+(90, 4561237894561, 'Cuadernos de 100 hojas', 'Cuaderno', 3000),
+(30, 7891234567891, 'Lápices HB', 'Lápiz HB', 10000),
+(1000,3216549871231, 'Mochilas escolares', 'Mochila', 500),
+(25,6549873214561, 'Gomas de borrar', 'Goma de borrar', 8000),
+(200,7894561237819, 'Calculadoras científicas', 'Calculadora', 1500),
+(23, 1237896543121, 'Cartulinas de colores', 'Cartulina', 6000),
+(15, 4567891231987, 'Pegamento en barra', 'Pegamento', 7000),
+(30, 1122334451566, 'Bolígrafos azules', 'Bolígrafo', 12000),
+(28, 2233445516677, 'Resaltadores', 'Resaltador', 8000),
+(60, 3344556617788, 'Libretas de notas', 'Libreta', 5000),
+(150, 4455667718899, 'Grampas para engrapadora', 'Grampas', 15000),
+(17, 5566778891900, 'Notas adhesivas', 'Post-it', 9000);
 
 INSERT INTO Clientes (razonSocial, direccion_Calle, direccion_Ciudad, direccion_Distancia, direccion_Numero, rut_Rut) VALUES
 ('Pepsico SA', 'Minas', 'Montevideo', 125, 4558, '123456789123'),
